1c11593b9501fbc9d2e3cf1a553b62b8fde458c5
Musa Khan <tamzid101@gmail.com>
2019-11-28 22:52:14 +0600

    pdf parser fptr bug fixed.

--- book_reader/parser.py
+++ book_reader/parser.py
@@ -5,10 +5,30 @@ class PDFParser:
     def __init__(self , path):
 
         self.reader_obj = None
+        self.num_page = -1
+        self.f_ptr = open(path , "rb")
 
-        with open(path , "rb") as f:
-            print("PDF reader object is {}".format(self.reader_obj))
-            print("Number of Pages : {}".format(self.reader_obj.getNumPages()))
-            print("Title : {}".format(self.reader_obj.getDocumentInfo().title))
-            print("Author : {}".format(self.reader_obj.getDocumentInfo().author))
+        self.reader_obj = PdfFileReader(self.f_ptr)
+        self.total_pages = self.reader_obj.getNumPages()
 
+
+
+    def get_text_from(self , page_num):
+
+        if page_num < 0 and page_num > self.total_pages:
+            print("Invalid page num : {}".format(page_num))
+            return
+
+        page = self.reader_obj.getPage(page_num)
+        return page.extractText()
+
+    def show_info(self):
+        print("Number of Pages : {}".format(self.total_pages))
+        print("Title : {}".format(self.reader_obj.getDocumentInfo().title))
+        print("Author : {}".format(self.reader_obj.getDocumentInfo().author))
+
+        print("Book Outlines")
+        self.get_text_from(10)
+
+    def close(self):
+        self.f_ptr.close()
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from book_reader.parser import PDFParser
 def main():
     #speak("This is a message.")
 
-    pdf_parser = PDFParser(path = "/home/musa/Downloads/webassembly-action-eexamples-emscripten.pdf")
+    pdf_parser = PDFParser(path = "/home/musa/Downloads/design-web-apis-arnaud-lauret.pdf")
+    print(pdf_parser.get_text_from(62))
 
+    pdf_parser.close()
 
 if __name__ == "__main__":
     main()
